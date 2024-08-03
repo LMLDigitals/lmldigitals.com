e241460966c242cf58f2c7b9ec61bd33876efafa
abduleyousuf08 <abduleyousuf08@gmail.com>
2024-08-03 13:34:57 +0300

    connected the google ad sense

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,38 +1,38 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import Footer from "./Footer";
-import "./globals.css";
-import Navbar from "./Navbar";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import Footer from './Footer';
+import './globals.css';
+import Navbar from './Navbar';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "LML Digitals | Marketing and Software Development",
+  title: 'LML Digitals | Marketing and Software Development',
   description:
-    "LML Digitals is a leading marketing and software development company dedicated to transforming businesses with innovative digital solutions.",
+    'LML Digitals is a leading marketing and software development company dedicated to transforming businesses with innovative digital solutions.',
   keywords:
-    "marketing, software development, digital solutions, business transformation, LML Digitals",
-  authors: [{ name: "LML Digitals", url: "https://www.lmldigitals.com" }],
-  viewport: "width=device-width, initial-scale=1.0",
+    'marketing, software development, digital solutions, business transformation, LML Digitals',
+  authors: [{ name: 'LML Digitals', url: 'https://www.lmldigitals.com' }],
+  viewport: 'width=device-width, initial-scale=1.0',
   openGraph: {
-    title: "LML Digitals | Marketing and Software Development",
+    title: 'LML Digitals | Marketing and Software Development',
     description:
-      "Transform your business with innovative digital solutions from LML Digitals, a leader in marketing and software development.",
-    type: "website",
-    url: "https://www.lmldigitals.com",
+      'Transform your business with innovative digital solutions from LML Digitals, a leader in marketing and software development.',
+    type: 'website',
+    url: 'https://www.lmldigitals.com',
     images: [
       {
-        url: "/app/opengraph-image.png",
+        url: '/app/opengraph-image.png',
       },
     ],
   },
   twitter: {
-    card: "summary_large_image",
-    site: "@LMLDigitals",
-    title: "LML Digitals | Marketing and Software Development",
+    card: 'summary_large_image',
+    site: '@LMLDigitals',
+    title: 'LML Digitals | Marketing and Software Development',
     description:
-      "Transform your business with innovative digital solutions from LML Digitals, a leader in marketing and software development.",
-    images: ["/images/twitter-image.jpg"], // Relative path to the image
+      'Transform your business with innovative digital solutions from LML Digitals, a leader in marketing and software development.',
+    images: ['/images/twitter-image.jpg'], // Relative path to the image
   },
 };
 
@@ -62,6 +62,15 @@ export default function RootLayout({
               gtag('js', new Date());
               gtag('config', 'G-DR17LYS00W');
             `,
+        }}
+      />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+        window.dataLayer = window.dataLayer || []; function gtag()
+        {dataLayer.push(arguments)} gtag('js', new Date()); gtag('config',
+        'AW-16648663241')
+        `,
         }}
       />
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,23 @@ import ContentFour from '@/components/ContentFour';
 import ContentSix from '@/components/ContentSix';
 import ContentThree from '@/components/ContentThree';
 import Hero from '@/components/Hero';
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <div className="flex flex-col gap-20">
+      <Head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-16648663241"
+        ></script>{' '}
+        <script>
+          {' '}
+          window.dataLayer = window.dataLayer || []; function gtag()
+          {dataLayer.push(arguments)} gtag('js', new Date()); gtag('config',
+          'AW-16648663241');{' '}
+        </script>
+      </Head>
       <Hero />
       <BrandsTrustUs />
       <ContentThree />
